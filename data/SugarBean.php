1353e8afcb53542368292289c6ed6d922440eb0f
hbartel <52085197+hbartel@users.noreply.github.com>
2022-10-27 15:06:05 -0500

    Update SugarBean.php

--- data/SugarBean.php
+++ data/SugarBean.php
@@ -873,7 +873,6 @@ class SugarBean
                     $final_query .= ' UNION ALL ( ' . $tmp_final_query . ' )';
                 } else {
                     $final_query_rows = '(' . $parentbean->create_list_count_query($tmp_final_query, $parameters) . ')';
-                    $final_query = '(' . $tmp_final_query . ')';
                     $first = false;
                 }
             }
@@ -927,7 +926,6 @@ class SugarBean
                     $query = ' UNION ALL ( ' . $query . ' )';
                     $final_query_rows .= " UNION ALL ";
                 } else {
-                    $query = '(' . $query . ')';
                     $first = false;
                 }
                 $query_array = $subquery['query_array'];
